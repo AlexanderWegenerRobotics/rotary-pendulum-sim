ec301128ff26d5d4a907f83121e148e3b9ae4dc2
ftq2003 <ftq2003@gmail.com>
2026-04-14 21:23:40 +0800

    commit: added iLQR controller

---
+++ controllers/__init__.py
@@ -0,0 +1,2 @@
+self._filtered_state = None
+self.alpha_v = 0.3  # Velocity filter coefficient (Lower = smoother but more lag)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import yaml
 import multiprocessing as mp
 
 from src.double_pendulum import DoublePendulum
-from controllers.spring_controller import SpringController
+from controllers.ilqr_controller import ILQRController
 
 
 def load_config(path: str = "config.yaml") -> dict:
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     mp.set_start_method("spawn", force=True)
 
     config = load_config()
-    controller = SpringController(config, kp=0.5, kd=2.0)
+    controller = ILQRController(config)
 
     env = DoublePendulum(config, controller)
     env.run()
